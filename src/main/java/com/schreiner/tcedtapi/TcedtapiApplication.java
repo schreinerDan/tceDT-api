9abbab4ebeeea3eac58a8405ba08e83a012a49ea
Daniel Schreiner <schreiner.daniel@gmail.com>
2021-10-26 13:57:36 -0300

    First Commit

---
+++ src/main/java/com/schreiner/tcedtapi/TcedtapiApplication.java
@@ -0,0 +1,13 @@
+package com.schreiner.tcedtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcedtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcedtapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schreiner/tcedtapi/TcedtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schreiner.tcedtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcedtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
